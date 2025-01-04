7b9f77ebb0aa1f254c3c7dba83b555e00ad35bf0
Daniel Noh <daniel@criosis.com>
2025-01-04 16:44:27 -0500

    Update: remove unnecesary key bindings

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -10,11 +10,11 @@ vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', 'p', '"_dp')
 
 -- Quickly clear search highlighting
-vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>') 
+vim.keymap.set('n', '<leader>k', ':nohlsearch<CR>')
 
 -- Easy insertion of a trailing ; or , from insert mode
-vim.keymap.set('i', ';;', '<Esc>A;')
-vim.keymap.set('i', ',,', '<Esc>A,')
+vim.keymap.set('i', '<leader>;', '<Esc>A;')
+vim.keymap.set('i', '<leader>,', '<Esc>A,')
 
 -- Move lines up and down
 vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
@@ -24,6 +24,3 @@ vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
 vim.keymap.set('n', '<A-j>', ':move .+1<CR>')
 vim.keymap.set('n', '<A-k>', ':move .-2<CR>')
 
-vim.keymap.set('i', '<A-w>', '<Esc>:w')
-
-vim.keymap.set('n', '<A-o>', '<Esc>:Neotree')
