c79f75366d6068378ae82988dedb3491ee4cfc0d
Daniel Noh <daniel@criosis.com>
2025-01-25 15:14:12 -0500

    chore: update plugin versions and refactor LSP configuration
    Update plugin versions in  and refactor LSP setup into modular files. Add new plugins for LSP management, autocompletion, and code trimming. Simplify and improve maintainability of the LSP configuration by splitting it into dedicated files for , , , and .

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,12 +1,18 @@
 vim.opt.expandtab = true
+vim.opt.smarttab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
 vim.opt.smartindent = true
 
+vim.opt.swapfile = false
+
 vim.opt.wrap = false
 vim.opt.breakindent = true
+
+-- Highlight column 81
+vim.opt.colorcolumn = '81'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -21,14 +27,29 @@ vim.opt.mouse = 'a'
 
 vim.opt.termguicolors = true
 
-vim.opt.spell = true
+-- Spell checking
+vim.opt.spell = false
+vim.opt.spelllang = {'en_us'}
+vim.api.nvim_create_autocmd(
+  {
+    'BufEnter',
+    'BufWinEnter'
+  },
+  {
+    pattern =  {
+      '*.md',
+    },
+    command = [[:setlocal spell]]
+  }
+)
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Enabled the below list of characters
-vim.opt.list = true
 vim.opt.listchars = { tab = '▸ ', trail = '·', nbsp = '␣' }
+vim.opt.list = true
+
 -- Remove the ~ from the end of the buffer
 vim.opt.fillchars:append({ eob = ' '})
 
@@ -43,10 +64,13 @@ vim.opt.clipboard = 'unnamedplus'
 
 -- Ask for confirmation instead of show error message
 vim.opt.confirm = true
+
 -- Persist undo
 vim.opt.undofile = true
+
 -- Automatically save a backup file
 vim.opt.backup = true
+
 -- Keep backups out of the current directory
 vim.opt.backupdir:remove('.')
 
